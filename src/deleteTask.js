a90c9d0646b3100e638daa11a68d15f84bf9053d
ricardo-vaca <ricardo.vaca@ioet.com>
2022-06-19 20:58:55 -0500

    Add function for delete a task 🔨. The end ✅

---
+++ src/deleteTask.js
@@ -0,0 +1,27 @@
+const AWS = require("aws-sdk");
+
+const deleteTask = async (event) => {
+  const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+  const { id } = event.pathParameters;
+
+  await dynamodb
+    .delete({
+      TableName: "TasksTable",
+      Key: {
+        id,
+      },
+    })
+    .promise();
+
+  return {
+    status: 200,
+    body: {
+      message: "Task deleted",
+    },
+  };
+};
+
+module.exports = {
+  deleteTask,
+};
